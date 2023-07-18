891da74ed09fad8a098386fdb762353f94ad8555
Davi Wentrick Feijó <davi.w.feijo@gmail.com>
2023-07-18 13:24:23 -0300

    lista discriminante

--- lists/lista_discriminante/lista_discriminante_script.R
+++ lists/lista_discriminante/lista_discriminante_script.R
@@ -1,4 +1,5 @@
-pacman::p_load(tidyverse,readxl)
+pacman::p_load(tidyverse,readxl,knitr,effectsize,DescTools,tidyverse,MASS,klaR,knitr,cowplot,nlme,
+               Rchoice,AICcmodavg,mdscore,questionr,mda,mvnTest,gclus,mclust,caTools)
 
 
 
@@ -87,16 +88,248 @@ BankruptcyData <- read_excel("data/BankruptcyData.xlsx") %>%
          x4 = as.numeric(x4))
 
 #a)
-plot(BankruptcyData)
+par(mfrow=c(1, 3))
+plot(BankruptcyData$x1,BankruptcyData$x2)
+plot(BankruptcyData$x1,BankruptcyData$x3)
+plot(BankruptcyData$x1,BankruptcyData$x4)
 
 
 #b)
 
-dados1 = BankruptcyData %>%
+dados_falido = BankruptcyData %>%
   filter(x5 == 1)
 
-dados2 = BankruptcyData %>%
-  filter(x5 == 2)
+dados_ativo = BankruptcyData %>%
+  filter(x5 == 0)
+
+x1_barra = c(mean(dados_ativo$x1),mean(dados_ativo$x2))
+
+x2_barra = c(mean(dados_falido$x1),mean(dados_falido$x2))
+
+s1 = round(cov(dados_ativo[,c(1,2)]),5)
+
+s2 = round(cov(dados_falido[,c(1,2)]),5)
 
 
+#c) 
+
+
+dados <- dados[,-6]
+gqda <- qda(x5~x1+x2, data = dados,prior =c(.5,.5))
+
+gqdap1 <- predict(gqda)
+gqctable1 <- table(dados$x5, gqdap1$class)
+
+# Com validação cruzada
+gqdaVC <- qda(x5~x1+x2, data = dados,prior =c(.5,.5),CV=T)
+
+
+
+
+#d)
+
+# Matrizes de confusão:
+M <- table(dados$x5, gqdap1$class) 
+MCV <- table(dados$x5, gqdaVC$class) 
+
+# APER e \hat{E}APR:
+APER <- (sum(M)-sum(diag(M)))/sum(M) # APER x_1,x_2
+E_APR <- (sum(MCV)-sum(diag(MCV)))/sum(MCV) # \hat{E} APR x_1,x_2
+
+
+#e)
+
+#dados <- dados[,-6]
+gqda <- qda(x5~x1+x2, data = dados,prior =c(.05,.95))
+
+gqdap1 <- predict(gqda)
+gqctable1 <- table(dados$x5, gqdap1$class)
+
+# Com validação cruzada
+gqdaVC <- qda(X5~X1+X2, data = dados,prior =c(.05,.95),CV=T)
+
+# Matrizes de confusão:
+M <- table(dados$X5, gqdap1$class) 
+MCV <- table(dados$X5, gqdaVC$class) 
+
+# APER e \hat{E}APR:
+APER <- (sum(M)-sum(diag(M)))/sum(M) # APER x_1,x_2
+E_APR <- (sum(MCV)-sum(diag(MCV)))/sum(MCV) # \hat{E} APR x_1,x_2
+
+#f)
+
+cov_pooled_inv = solve(cov_pooled(dados_ativo[,c(1,2)],dados_falido[,c(1,2)]))
+
+a <- t(x1_barra - x2_barra) %*% cov_pooled_inv
+m <- 0.5*(a %*% x1_barra+ a %*% x2_barra)
+
+pop <- BankruptcyData %>%
+  rowwise() %>%
+  mutate(M = a %*% c(x1, x2)) %>%
+  mutate(pop = ifelse(M > m,0,1)) 
+  
+matriz = pop %>%
+  group_by(x5,pop) %>%
+  summarise(freq = n())
+matriz
+
+9/46
+
+#g)
+
+# x1,x3
+
+x1_barra = c(mean(dados_ativo$x1),mean(dados_ativo$x3))
+
+x2_barra = c(mean(dados_falido$x1),mean(dados_falido$x3))
+
+s1 = round(cov(dados_ativo[,c(1,3)]),5)
+
+s2 = round(cov(dados_falido[,c(1,3)]),5)
+
+#analise com 0.5\0.5
+
+#dados <- dados[,-6]
+gqda <- qda(x5~x1+x3, data = dados,prior =c(.5,.5))
+
+gqdap1 <- predict(gqda)
+gqctable1 <- table(dados$x5, gqdap1$class)
+
+# Com validação cruzada
+gqdaVC <- qda(x5~x1+x3, data = dados,prior =c(.5,.5),CV=T)
+
+# Matrizes de confusão:
+M <- table(dados$x5, gqdap1$class) 
+MCV <- table(dados$x5, gqdaVC$class) 
+
+# APER e \hat{E}APR:
+APER <- (sum(M)-sum(diag(M)))/sum(M) # APER x_1,x_2
+E_APR <- (sum(MCV)-sum(diag(MCV)))/sum(MCV) # \hat{E} APR x_1,x_2
+
+
+#analise com 0.05\0.95
+
+#dados <- dados[,-6]
+gqda <- qda(x5~x1+x3, data = dados,prior =c(.05,.95))
+
+gqdap1 <- predict(gqda)
+gqctable1 <- table(dados$x5, gqdap1$class)
+
+# Com validação cruzada
+gqdaVC <- qda(x5~x1+x3, data = dados,prior =c(.05,.95),CV=T)
+
+# Matrizes de confusão:
+M <- table(dados$x5, gqdap1$class) 
+MCV <- table(dados$x5, gqdaVC$class) 
+
+# APER e \hat{E}APR:
+APER <- (sum(M)-sum(diag(M)))/sum(M) # APER x_1,x_2
+E_APR <- (sum(MCV)-sum(diag(MCV)))/sum(MCV) # \hat{E} APR x_1,x_2
+
+
+# x1,x4
+
+x1_barra = c(mean(dados_ativo$x1),mean(dados_ativo$x4))
+
+x2_barra = c(mean(dados_falido$x1),mean(dados_falido$x4))
+
+s1 = round(cov(dados_ativo[,c(1,4)]),5)
+
+s2 = round(cov(dados_falido[,c(1,4)]),5)
+
+#analise com 0.5\0.5
+
+#dados <- dados[,-6]
+gqda <- qda(x5~x1+x4, data = dados,prior =c(.5,.5))
+
+gqdap1 <- predict(gqda)
+gqctable1 <- table(dados$x5, gqdap1$class)
+
+# Com validação cruzada
+gqdaVC <- qda(x5~x1+x4, data = dados,prior =c(.5,.5),CV=T)
+
+# Matrizes de confusão:
+M <- table(dados$x5, gqdap1$class) 
+MCV <- table(dados$x5, gqdaVC$class) 
+
+# APER e \hat{E}APR:
+APER <- (sum(M)-sum(diag(M)))/sum(M) # APER x_1,x_2
+E_APR <- (sum(MCV)-sum(diag(MCV)))/sum(MCV) # \hat{E} APR x_1,x_2
+
+#analise com 0.05\0.95
+
+#dados <- dados[,-6]
+gqda <- qda(x5~x1+x4, data = dados,prior =c(.05,.95))
+
+gqdap1 <- predict(gqda)
+gqctable1 <- table(dados$x5, gqdap1$class)
+
+# Com validação cruzada
+gqdaVC <- qda(x5~x1+x4, data = dados,prior =c(.05,.95),CV=T)
+
+# Matrizes de confusão:
+M <- table(dados$x5, gqdap1$class) 
+MCV <- table(dados$x5, gqdaVC$class) 
+
+# APER e \hat{E}APR:
+APER <- (sum(M)-sum(diag(M)))/sum(M) # APER x_1,x_2
+E_APR <- (sum(MCV)-sum(diag(MCV)))/sum(MCV) # \hat{E} APR x_1,x_2
+
+#### questa0 90
+
+
+# Discriminante linear
+data(bank)
+bank$Status <- factor(bank$Status)
+
+LDA <- lda(Status~., data = bank,prior=c(.5,.5))
+LDAp1 <- predict(LDA)
+LDAtable1 <- table(bank$Status, LDAp1$class)
+prop1 <- (diag(prop.table(LDAtable1,1))) # prop de classif. correta no grupo
+propt1 <- (sum(diag(prop.table(LDAtable1)))) # prop total de classf. correta 
+
+# Matrizes de confusão:
+M <- table(bank$Status, LDAp1$class) 
+
+# APER:
+APER <- (sum(M)-sum(diag(M)))/sum(M)
+
+#partimat(Status ~ ., data = bank, method = "lda")
+
+# Discriminante quadrático
+QDA <- qda(Status~., data = bank,prior=c(.5,.5))
+QDAp1 <- predict(QDA)
+QDAtable1 <- table(bank$Status, QDAp1$class)
+prop2 <- (diag(prop.table(QDAtable1,1))) # prop de classif. correta no grupo
+propt2 <- (sum(diag(prop.table(QDAtable1)))) # prop total de classf. correta 
+
+# Matrizes de confusão:
+M1 <- table(bank$Status, QDAp1$class) 
+
+# APER:
+APER1 <- (sum(M1)-sum(diag(M1)))/sum(M1)
+
+#partimat(Status ~ ., data = bank, method = "qda")
+
+# Análise de discriminantes por mistura de normais (mclust)
+
+Class <- factor(bank$Status, levels = 0:1,
+                labels = c("Genuína", "Falsificada"))
+
+X <- data.matrix(bank[,-1])
+
+mod <- Mclust(X)
+summary(mod$BIC)
+
+plot(mclustBIC(X))
+
+summary(mod)
+table(Class, mod$classification)    # por algum motivo este não renderiza
+RAND <- adjustedRandIndex(Class, mod$classification)
+
+# Matrizes de confusão:
+M2 <- table(bank$Status, mod$class) 
+
+# APER:
+APER2 <- (2+16)/(2+98+16+84)
 
